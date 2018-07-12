f5a47b6b443301a8ebfeb27eb275d90579ff27fc
Vlad Doster <dosterm@wit.edu>
2018-07-12 13:26:31 -0400

    Removed dist

--- frontend/src/components/Visualizations/Visualizations.vue
+++ frontend/src/components/Visualizations/Visualizations.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
     <svg @mousemove="mouseover" :width="width" :height="height">
-      <g :style="{transform: `translate(${margin.left}px, ${margin.top}px)`}">
-        <path class="area" :d="paths.area" />
-        <path class="line" :d="paths.line" />
-        <path class="selector" :d="paths.selector" />
-      </g>
     </svg>
   </div>
 </template>
